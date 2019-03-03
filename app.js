c6285d05ac6b888dd2f084533d872a4ec94ffb38
James Gary <gmarchesi888@gmail.com>
2019-03-03 17:53:36 -0500

    deleted some unnecessary comments.

--- app.js
+++ app.js
@@ -6,10 +6,3 @@ app.get("/", (req, res) => res.send("Hello world"));
 
 app.listen(port, () => console.log("Listening on port " + port));
 
-// const express = require("express");
-// const app = express();
-// const port = 3000;
-
-// app.get("/", (req, res) => res.send("Hello World!"));
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
